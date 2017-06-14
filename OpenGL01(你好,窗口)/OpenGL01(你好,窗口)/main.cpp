6ee75151e6bdc1d53928f8927c20858fc4dc8360
123 <fusheng_it@163.com>
2017-06-14 10:33:13 +0800

    新建你好,窗口项目

---
+++ OpenGL01(你好,窗口)/OpenGL01(你好,窗口)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL01(你好,窗口)
+//
+//  Created by 123 on 17/6/14.
+//  Copyright © 2017年 MakeKeyReuseable. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
